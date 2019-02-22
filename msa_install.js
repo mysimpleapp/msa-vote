acbf4662acb1bbae96506954e458141fc743ad84
Nicoco220983 <nicolas.carrez@gmail.com>
2019-02-22 23:02:20 +0100

    Simplified msa_start & msa_install

--- msa_install.js
+++ msa_install.js
@@ -1,11 +1,7 @@
-module.exports = async (itf, next) => {
-	try {
-		// create table in DB
-		await itf.installMsaMod("db", "msa-db")
-		const { VotesDb, VoteVoicesDb } = require("./db")
-		await VotesDb.sync()
-		await VoteVoicesDb.sync()
-	} catch(err) { return next(err) }
-	next()
+module.exports = async itf => {
+	// create table in DB
+	const { VotesDb, VoteVoicesDb } = require("./db")
+	await VotesDb.sync()
+	await VoteVoicesDb.sync()
 }
 
